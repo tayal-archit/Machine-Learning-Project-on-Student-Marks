d5e7939d879b09bf502a4f52749009d269cdb48b
Archit <atayalwork@gmail.com>
2024-07-03 17:25:08 +0530

    deploying

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData, predict
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect("/predictdata")
 
 @app.route('/predictdata', methods = ['GET', 'POST'])
 def predict_datapoint():
@@ -29,8 +30,11 @@ def predict_datapoint():
         pred_df = data.get_data_as_data_frame()
         print(pred_df)
         
-        results = predict(pred_df)
-        return render_template('home.html', results=results[0])
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
     
 if __name__ == "__main__":
-    app.run(host= "0.0.0.0", debug=True)
+    app.run(host= "0.0.0.0")
